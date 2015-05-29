9dfa2c134f7df498b398948c448e877697d6cfb0
Martin Møller <mmj@dbc.dk>
2015-05-29 16:13:08 +0200

    dummy data updated

--- examples/app.js
+++ examples/app.js
@@ -3,7 +3,50 @@
 import React from 'react';
 import AutoComplete from '../src/components/AutoComplete/AutoComplete.component.js';
 
-const _DATA = [{label: 'author', data: ['Test Hest', 'Hest Hest', 'Fest Hest', 'Gæst Hest']}, {label: 'author', data: ['Test Hest', 'Hest Hest', 'Fest Hest', 'Gæst Hest']}];
+const _DATA = [
+  {
+    label: 'Title',
+    data: [
+      {
+        text: 'Test Hest',
+        img: 'http://dummyimage.com/50x50/000/fff'
+      },
+      {
+        text: 'Hest Hest',
+        img: 'http://dummyimage.com/50x50/000/fff'
+      },
+      {
+        text: 'Fest Hest',
+        img: 'http://dummyimage.com/50x50/000/fff'
+      },
+      {
+        text: 'Gæst Hest',
+        img: 'http://dummyimage.com/50x50/000/fff'
+      }
+    ]
+  },
+  {
+    label: 'Author',
+    data: [
+      {
+        text: 'Test Hest',
+        img: 'http://dummyimage.com/50x50/000/fff'
+      },
+      {
+        text: 'Hest Hest',
+        img: ''
+      },
+      {
+        text: 'Fest Hest',
+        img: ''
+      },
+      {
+        text: 'Gæst Hest',
+        img: ''
+      }
+    ]
+  }
+];
 
 let SearchField = React.createClass({
   render() {

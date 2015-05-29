ea2892a99f517a211ff6b4ffbdf1569df9eb332e
Martin Møller <mmj@dbc.dk>
2015-05-29 16:12:26 +0200

    image added to auto-complete row

--- src/components/AutoCompleteRow/AutoCompleteRow.component.js
+++ src/components/AutoCompleteRow/AutoCompleteRow.component.js
@@ -1,12 +1,18 @@
 'use strict';
 import React from 'react';
 
+import './AutoCompleteRow.scss';
+
 var AutoCompleteRow = React.createClass({
   render() {
     const text = this.props.text || '';
+    const image = this.props.image || '';
     return (
       <div className='autocomplete--row'>
-        <span>{text}</span>
+        <a href="#">
+          <div className='autocomplete--row-image'><img src={image}/></div>
+          <div className='autocomplete--row-text'>{text}</div>
+        </a>
       </div>
     );
   }

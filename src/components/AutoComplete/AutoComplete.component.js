9313b324e5b3554116acc606edac650237abd25c
Martin Møller <mmj@dbc.dk>
2015-06-08 14:44:22 +0200

    docs

--- src/components/AutoComplete/AutoComplete.component.js
+++ src/components/AutoComplete/AutoComplete.component.js
@@ -1,11 +1,15 @@
 'use strict';
 
+/**
+ * @file Main autocomplete component
+ */
+
 import React from 'react';
 import AutoCompleteCategory from './../AutoCompleteCategory/AutoCompleteCategory.component.js';
 
-let AutoComplete = React.createClass({
+export default React.createClass({
   render() {
-    let categories = this._getCategories();
+    const categories = this._getCategories();
     let classNames = 'autocomplete--container';
     classNames += (this.props.visible === true) ? '' : ' autocomplete--container-hidden';
 
@@ -16,9 +20,16 @@ let AutoComplete = React.createClass({
     );
   },
 
+  /*
+   * Retrieves the categories from the data prop and renders a
+   * AutoCompleteCategory for each category.
+   *
+   * @return {Array}
+   */
   _getCategories() {
+
     let categories = [];
-    let data = this.props.data || [];
+    const data = this.props.data || [];
     data.forEach((value, key) => {
       categories.push(
         <AutoCompleteCategory key={key} label={value.label} data={value.data}/>);
@@ -32,5 +43,3 @@ let AutoComplete = React.createClass({
     data: React.PropTypes.array
   }
 });
-
-export default AutoComplete;

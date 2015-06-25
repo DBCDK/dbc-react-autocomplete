3181355a4eb255b91cde5138c6f40d1911a0a892
Martin Møller <mmj@dbc.dk>
2015-06-25 10:19:10 +0200

    changes made according to review

--- src/components/AutoComplete/AutoComplete.component.js
+++ src/components/AutoComplete/AutoComplete.component.js
@@ -9,16 +9,9 @@ import {forIn} from 'lodash';
 import AutoCompleteCategory from './../AutoCompleteCategory/AutoCompleteCategory.component.js';
 
 export default React.createClass({
-  render() {
-    const categories = this._getCategories();
-    let classNames = 'autocomplete--container';
-    classNames += (this.props.visible === true) ? '' : ' autocomplete--container-hidden';
-
-    return (
-      <div className={classNames}>
-        {categories}
-      </div>
-    );
+  propTypes: {
+    visible: React.PropTypes.bool,
+    data: React.PropTypes.object
   },
 
   /*
@@ -27,20 +20,28 @@ export default React.createClass({
    *
    * @return {Array}
    */
-  _getCategories() {
+  getCategories() {
 
     let categories = [];
     const data = this.props.data || {};
 
     forIn(data, (value, key) => {
-      categories.splice(value.weight, 0, <AutoCompleteCategory key={key} label={value.label} data={value.data}/>);
+      categories.splice(value.weight, 0,
+        <AutoCompleteCategory key={key} label={value.label} data={value.data} />);
     });
 
     return categories;
   },
 
-  propTypes: {
-    visible: React.PropTypes.bool,
-    data: React.PropTypes.object
+  render() {
+    const categories = this.getCategories();
+    let classNames = 'autocomplete--container';
+    classNames += (this.props.visible === true) ? '' : ' autocomplete--container-hidden';
+
+    return (
+      <div className={classNames} >
+        {categories}
+      </div>
+    );
   }
 });

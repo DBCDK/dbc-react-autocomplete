819d0a3f4a8c542747cf96efb43fcab8f740bd8b
Martin Møller <mmj@dbc.dk>
2015-06-25 10:23:55 +0200

    restructured class

--- src/components/AutoCompleteRow/AutoCompleteRow.component.js
+++ src/components/AutoCompleteRow/AutoCompleteRow.component.js
@@ -2,34 +2,34 @@
 import React from 'react';
 
 var AutoCompleteRow = React.createClass({
+  propTypes: {
+    text: React.PropTypes.string,
+    image: React.PropTypes.string
+  },
+
+  getImage(image) {
+    return (
+      <div className='autocomplete--row-image' ><img src={image} /></div>
+    );
+  },
+
   render() {
     const text = this.props.text || '';
     const image = this.props.image || null;
     let imageElement = null;
 
     if (image) {
-      imageElement = this._getImage(image);
+      imageElement = this.getImage(image);
     }
 
     return (
-      <div className='autocomplete--row'>
-        <a href="#">
+      <div className='autocomplete--row' >
+        <a href="#" >
           {imageElement}
-          <div className='autocomplete--row-text'>{text}</div>
+          <div className='autocomplete--row-text' >{text}</div>
         </a>
       </div>
     );
-  },
-
-  _getImage(image) {
-    return (
-      <div className='autocomplete--row-image'><img src={image}/></div>
-    );
-  },
-
-  propTypes: {
-    text: React.PropTypes.string,
-    image: React.PropTypes.string
   }
 });
 
